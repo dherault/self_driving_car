3dd7ab08e640e741646de0e53804abffdbaebc4a
David Hérault <dherault@gmail.com>
2019-03-25 15:26:46 +0100

    draw car

--- client/src/lib/draw.js
+++ client/src/lib/draw.js
@@ -1,17 +1,26 @@
 import state from './state'
 
-function drawCar(_, angle) {
+function drawCar(_, offset) {
+  const { width, height, position, angle } = state.car
 
+  _.fillStyle = 'Navy'
+  _.save()
+  _.translate(position[0] + offset[0], position[1] + offset[1])
+  _.rotate(angle)
+  _.fillRect(-width / 2, -height / 2, width, height)
+  _.fillStyle = 'Crimson'
+  _.fillRect(-width / 2, -height / 2, width, height / 5)
+  _.restore()
 }
 
 function draw(_) {
-
+  const { world } = state
   const { width, height } = _.canvas
 
   _.clearRect(0, 0, width, height)
 
-  const marginX = (width - 2 * state.worldWidth) / 3
-  const marginY = (height - state.worldHeight) / 2
+  const marginX = (width - 2 * world.width) / 3
+  const marginY = (height - world.height) / 2
 
   /* ------------------
     Draw model world
@@ -20,7 +29,7 @@ function draw(_) {
   _.strokeStyle = 'black'
   _.beginPath()
 
-  state.worldSegments.forEach(([p1, p2]) => {
+  world.segments.forEach(([p1, p2]) => {
     _.moveTo(marginX + p1[0], marginY + p1[1])
     _.lineTo(marginX + p2[0], marginY + p2[1])
   })
@@ -33,7 +42,13 @@ function draw(_) {
   ------------------- */
 
   _.strokeStyle = 'red'
-  _.strokeRect(2 * marginX + state.worldWidth, marginY, state.worldWidth, state.worldHeight)
+  _.strokeRect(2 * marginX + world.width, marginY, world.width, world.height)
+
+  /* ---------
+    Draw car
+  --------- */
+
+  drawCar(_, [marginX, marginY])
 }
 
 export default draw
--- client/src/lib/state.js
+++ client/src/lib/state.js
@@ -1,18 +1,30 @@
 const worldWidth = 400
 const worldHeight = 400
+const carWidth = 10
+const carHeight = 20
 
 const state = {
-  worldWidth,
-  worldHeight,
-  worldSegments: [
-    [[0, 0], [worldWidth, 0]],
-    [[worldWidth, 0], [worldWidth, worldHeight]],
-    [[worldWidth, worldHeight], [0, worldHeight]],
-    [[0, worldHeight], [0, 0]],
-    [[worldWidth / 3, worldHeight / 2], [ 2 * worldWidth / 3, worldHeight / 2]],
-    [[worldWidth / 2, worldHeight / 8], [worldWidth / 2, 3 * worldHeight / 8]],
-    [[worldWidth / 2, 5 * worldHeight / 8], [worldWidth / 2, 7 * worldHeight / 8]],
-  ],
+  world: {
+    width: worldWidth,
+    height: worldHeight,
+    segments: [
+      [[0, 0], [worldWidth, 0]],
+      [[worldWidth, 0], [worldWidth, worldHeight]],
+      [[worldWidth, worldHeight], [0, worldHeight]],
+      [[0, worldHeight], [0, 0]],
+      [[worldWidth / 3, worldHeight / 2], [ 2 * worldWidth / 3, worldHeight / 2]],
+      [[worldWidth / 2, worldHeight / 8], [worldWidth / 2, 3 * worldHeight / 8]],
+      [[worldWidth / 2, 5 * worldHeight / 8], [worldWidth / 2, 7 * worldHeight / 8]],
+    ],
+  },
+  car: {
+    width: carWidth,
+    height: carHeight,
+    angle: 0,
+    position: [worldWidth / 3, worldHeight / 3],
+    speed: 0,
+    wheelsAngle: 0,
+  },
 }
 
 export default state
